41bed4da12b3184dbb9c68a579ce6372f86d0b80
José Luis Neira Núñez <joseluisnn83@gmail.com>
2017-01-10 19:25:04 +0100

    Initial commit

---
+++ app/src/main/java/net/sourceforge/simcpux/Constants.java
@@ -0,0 +1,13 @@
+package net.sourceforge.simcpux;
+
+public class Constants {
+	// APP_ID �滻Ϊ���Ӧ�ôӹٷ���վ���뵽�ĺϷ�appId
+    //public static final String APP_ID = "wxa82b459fc8067511";
+	public static final String APP_ID = "wxd930ea5d5a258f4f";
+    
+    public static class ShowMsgActivity {
+		public static final String STitle = "showmsg_title";
+		public static final String SMessage = "showmsg_message";
+		public static final String BAThumbData = "showmsg_thumb_data";
+	}
+}
